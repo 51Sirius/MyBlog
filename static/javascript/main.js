314affdba87a1bd5a426488a8e8dd02621732502
Danil <71227522+51sirius@users.noreply.github.com>
2021-04-17 22:08:17 +0300

    Add 3d effect for card

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,8 +1,13 @@
-const card = document.querySelectorAll('.card')
-card.addEventListener('mousemove', rotate())
+const cards = document.querySelectorAll('.card')
+
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    card.addEventListener('mousemove', rotate);
+}
 
 function rotate(event) {
     const cardItem = this.querySelector('.card-item');
-    cardItem.innerText = event.offsetY;
-    console.log(event.offsetY)
+    const halfHeight = cardItem.offsetHeight / 2;
+    const halfWidth = cardItem.offsetWidth / 2;
+    cardItem.style.transform = 'rotateX(' + -(event.offsetY - halfHeight) / 6 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 6 + 'deg)';
 }
