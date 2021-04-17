a19dad464562743c6f337ffdbf570e62d35d0985
Danil <71227522+51sirius@users.noreply.github.com>
2021-04-17 22:10:47 +0300

    Stop rotate funct for cards

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -3,6 +3,7 @@ const cards = document.querySelectorAll('.card')
 for (let i = 0; i < cards.length; i++) {
     const card = cards[i];
     card.addEventListener('mousemove', rotate);
+    card.addEventListener('mouseout', stopRotate)
 }
 
 function rotate(event) {
@@ -10,4 +11,8 @@ function rotate(event) {
     const halfHeight = cardItem.offsetHeight / 2;
     const halfWidth = cardItem.offsetWidth / 2;
     cardItem.style.transform = 'rotateX(' + -(event.offsetY - halfHeight) / 6 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 6 + 'deg)';
+}
+function stopRotate(event){
+    const cardItem = this.querySelector('.card-item')
+    cardItem.style.transform = 'rotateX(0) rotateY(0)'
 }
