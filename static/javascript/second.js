c3fce9e848b84764302193acf4f80258b54425c3
Danil <71227522+51sirius@users.noreply.github.com>
2021-04-21 20:34:37 +0300

    Add fuct for delete background body

--- static/javascript/second.js
+++ static/javascript/second.js
@@ -1,10 +1,13 @@
 const card = document.querySelector('#spaceBot')
 
 card.addEventListener('mousemove', evt => create_bg(evt, 'https://www.bestsiling.ru/images/kosmos/19.jpg'));
-card.addEventListener('mouseout', );
+card.addEventListener('mouseout', delete_bg);
 
 
 function create_bg(event, image) {
-    const bg = document.querySelector('body')
-    bg.style.backgroundImage = 'url(' + image + ') !important;'
+    document.body.style.backgroundImage = 'url("' + image + '")'
+}
+
+function delete_bg(event) {
+    document.body.style.backgroundImage = 'linear-gradient( 109.6deg,  rgba(14,11,56,1) 11.2%, rgb(2, 81, 109) 91.1% )'
 }
