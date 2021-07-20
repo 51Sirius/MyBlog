9a36452e6e8b1e7b41335259d7207e6abd64463f
Danil <71227522+51sirius@users.noreply.github.com>
2021-07-20 21:41:31 +0300

    Add card scale effects

---
+++ static/javascript/cards.js
@@ -0,0 +1,23 @@
+const cards = document.querySelectorAll('.card')
+
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    card.addEventListener('mousemove', rising);
+    card.addEventListener('mouseout', stopRising);
+}
+
+function rising(event) {
+    const card = this
+    card.style.border = "3px solid #FAFAFA"
+    card.style.transform = 'scale(1.2)'
+    card.style.boxShadow = '0 0 150px #000000'
+
+
+}
+
+function stopRising(event) {
+    const card = this
+    card.style.border = "1px solid #FAFAFA"
+    card.style.transform = 'scale(1)'
+    card.style.boxShadow = '0 0 0'
+}
